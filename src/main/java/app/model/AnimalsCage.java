1bd810828f85d1a9da47e3cf9f435f8c39067d0d
smjavac <sm.yusupov@mail.ru>
2024-06-04 17:34:47 +0300

    bean

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class AnimalsCage {
     @Autowired
-    private Timer timer = new Timer();
+    private Timer timer;
 
     @Autowired
     @Qualifier("dog")
